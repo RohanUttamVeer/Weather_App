14bc3b64419a7f17f4ca8c03a7de90cbf0352b5d
RohanUttamVeer <rohanveer04@gmail.com>
2021-07-09 18:33:32 +0530

    data from json

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -9,24 +11,43 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=546f892f6984d3c99c996a504172f711');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      int condition = decodedData['weather'][0]['id'];
+      double temperature = decodedData['main']['temp'];
+      String city = decodedData['name'];
+
+      print(condition);
+      print(temperature);
+      print(city);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        // ignore: deprecated_member_use
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('get  location'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
--- scratch.dart
+++ scratch.dart
@@ -1,13 +1,12 @@
-import 'dart:io';
-
 void main() {
   performTask();
 }
 
-void performTask() {
+void performTask() async {
   task1();
-  task2();
-  task3();
+  print(task2());
+  // String task2Result = await task2();
+  // task3(task2Result);
 }
 
 void task1() {
@@ -15,12 +14,19 @@ void task1() {
   print('Task 1 complete');
 }
 
-void task2() {
-  String result = 'task 2 data';
-  print('Task 2 complete');
+Future<String> task2() async {
+  Duration threeSeconds = Duration(seconds: 3);
+  String result = '';
+  await Future.delayed(threeSeconds, () {
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+  return result;
 }
 
-void task3() {
+void task3(String task2Data) {
+  // Duration threeSeconds = Duration(seconds: 2);
+  // sleep(threeSeconds);
   String result = 'task 3 data';
-  print('Task 3 complete');
+  print('Task 3 complete with $task2Data');
 }
